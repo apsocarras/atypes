0dfcf361b919fd7817d617578d102e4f08fe6286
Alexander Socarras <apsocarras@gmail.com>
2025-10-15 12:04:24 -0400

    expose types in main __init__.py

--- src/atypes/__init__.py
+++ src/atypes/__init__.py
@@ -1,2 +1,49 @@
-def hello() -> str:
-    return "Hello from atypes!"
+from ._types import (
+    HasHeaders,
+    HasHeadersAndArgs,
+    HasHeadersAndRaw,
+    HasHeadersBody,
+    JSONScalar,
+    JSONType,
+    LoggerEvent,
+    NotImplementSentinel,
+    OmittedDefaultSentinel,
+    SentinelMeta,
+    SequenceNotStr,
+)
+from .enums import (
+    BaseStrEnum,
+    HttpMethod,
+    SerialFormatType,
+    SuccessStatus,
+)
+from .wrappers import (
+    ByteWrapperProto,
+    HtmlBytes,
+    JsonBytes,
+    MapString,
+    OtherBytes,
+)
+
+__all__ = [
+    "JSONScalar",
+    "JSONType",
+    "HasHeaders",
+    "HasHeadersAndRaw",
+    "HasHeadersBody",
+    "HasHeadersAndArgs",
+    "SequenceNotStr",
+    "SentinelMeta",
+    "OmittedDefaultSentinel",
+    "NotImplementSentinel",
+    "LoggerEvent",
+    "ByteWrapperProto",
+    "JsonBytes",
+    "HtmlBytes",
+    "OtherBytes",
+    "MapString",
+    "BaseStrEnum",
+    "SuccessStatus",
+    "SerialFormatType",
+    "HttpMethod",
+]
