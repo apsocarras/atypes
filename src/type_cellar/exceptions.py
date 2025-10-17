5edd90bf0b5179d189c57c92f6c2d6125fa07779
Alexander Socarras <apsocarras@gmail.com>
2025-10-17 10:41:09 -0400

    add custom exceptions

---
+++ src/type_cellar/exceptions.py
@@ -0,0 +1,38 @@
+# pyright: reportPrivateUsage=false
+from typing import Any
+
+from ._types import SequenceNotStr as Sequence
+from ._types import _VersionStampErrorArgs
+
+
+class ValidationError(BaseException):
+    """BaseException for violating class invariants"""
+
+    pass
+
+
+class VersionStampError(ValidationError):
+    """Failure to reconstruct original name from stamped name"""
+
+    def __init__(
+        self,
+        info: _VersionStampErrorArgs,
+        *args: object,
+        **kwargs: object,
+    ) -> None:
+        stamped_name = info["stamped_name"]
+        raw_name = info["raw_name"]
+        unstamped_name = info["unstamped_name"]
+
+        msg = f"Failed to reconstruct name: 'raw'={raw_name}, 'stamped_name'={stamped_name}, 'unstamped'={unstamped_name}"
+        super().__init__(msg, *args, **kwargs)
+
+
+class HeaderAndValuesError(ValidationError):
+    """Mismatches between headers and values"""
+
+    def __init__(
+        self, headers: Sequence[str], values: Sequence[str], *args: Any, **kwargs: Any
+    ) -> None:
+        msg = f"Length mismatch: 'headers'={len(headers)}, 'values'={len(values)}"
+        super().__init__(msg, *args, **kwargs)
