337c516fbd5a427a2be3b247c6de4550d02ea8fa
Alexander Socarras <apsocarras@gmail.com>
2025-10-17 10:24:58 -0400

    add models folder

---
+++ src/type_cellar/models/etl.py
@@ -0,0 +1,16 @@
+"""Common types used when writing data pipelines"""
+
+import attr
+
+
+@attr.define
+class BigQueryTableInfo:
+    """Type for a table in BigQuery"""
+
+    project_id: str
+    dataset_id: str
+    table_id: str
+
+    @property
+    def full_table_id(self) -> str:
+        return f"{self.project_id}.{self.dataset_id}.{self.table_id}"
